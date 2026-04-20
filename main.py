7206cf86afb45bfc8423cbb3ae8e54a0bf758e43
Guilherme Lima De Souza <guilhermel@positivo.com.br>
2026-04-20 03:44:13 -0400

    msg de aguarde ao executar app

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from app import App
 import customtkinter as ctk
+import time
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
+
+print("Abrindo aplicação... aguarde")
 
 if __name__ == "__main__":
     app = App()
