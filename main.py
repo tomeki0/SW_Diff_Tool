dfd61c3ba1ef36c1ad09eaa402bf7e52041532cb
Guilherme Lima De Souza <guilhermel@positivo.com.br>
2026-04-20 17:55:02 -0400

    msg de abrindo app removida

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import time
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("blue")
 
-print("Abrindo aplicação... aguarde")
-
 if __name__ == "__main__":
     app = App()
     app.mainloop()
